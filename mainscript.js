dbe28d800380f3f6fdf0440cbbb29109239a4ef3
chargercreepy001 <109840908+chargercreepy001@users.noreply.github.com>
2022-10-07 23:14:00 +0530

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -23,7 +23,7 @@ client.on('messageCreate', async message =>
   const sleep = ms => new Promise(interval => setTimeout(interval, ms));
   if (cmd.startsWith('>ban')||cmd.startsWith('>kick')||cmd.startsWith('>mute')||cmd.startsWith('>stfu')||cmd.startsWith('>getfucked')||cmd.startsWith('>membercount'))
     {
-      return message.reply("`Bot is under maintenance. this is not an error.```` dev news: Bot will be offline under maintenance for some time, please be patient and wait until maintenance is completed! ``` support server: https://discord.gg/eMR9euACtx");
+      return message.reply("`Bot is under maintenance. this is not an error.`  ``` dev news: Bot will be offline under maintenance for some time, please be patient and wait until maintenance is completed! ``` support server: https://discord.gg/eMR9euACtx");
       sleep(3000);
     }
   
