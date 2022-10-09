47c6d089300bf76f2bb1cf9681aa60756a0112a0
chargercreepy001 <109840908+chargercreepy001@users.noreply.github.com>
2022-10-09 13:17:37 +0530

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -21,11 +21,34 @@ client.on('messageCreate', async message =>
   let cmd = message.content.toLowerCase();
   
   const sleep = ms => new Promise(interval => setTimeout(interval, ms));
-  if (cmd.startsWith('>ban')||cmd.startsWith('>kick')||cmd.startsWith('>mute')||cmd.startsWith('>stfu')||cmd.startsWith('>getfucked')||cmd.startsWith('>membercount'))
+  if (cmd.startsWith('>ban')||cmd.startsWith('>kick')||cmd.startsWith('>mute')||cmd.startsWith('>help')||cmd.startsWith('>getfucked')||cmd.startsWith('>membercount'))
     {
       return message.reply("`Bot is under maintenance. this is not an error.`  ``` dev news: Bot will be offline under maintenance for some time, please be patient and wait until maintenance is completed! ``` support server: https://discord.gg/eMR9euACtx");
       sleep(3000);
     }
+  
+  details();
+async function details()
+{
+  
+  let Heathstatus = ':heart_decoration:**Very_Healthy**';
+  const ApiLatency = Math.round(client.ws.ping);
+
+  const botchannelinfo = client.channels.cache.get('1028357471372324895');
+  if (ApiLatency > 2000) Heathstatus = ':broken_heart: Not Fine';
+
+ 
+  if (botchannelinfo && botchannelinfo.isTextBased())
+  {
+    const mes =  botchannelinfo.messages.fetch('1028564046246596659').then(msg =>  msg.edit({embeds: [{
+      title: 'Bot status [updated every 3 seconds]',
+      description: `API Websocket Latency: **${ApiLatency}ms**\n Heath status: ${Heathstatus} \n My server count: **${client.guilds.cache.size}**`
+    }]}))
+  
+  }
+}
+
+setTimeout(details(), 3000);
   
 })
 client.login(process.env.mtoken);
